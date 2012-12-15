638a2a65b2812980bb04cabfbd32f126a9b2d3e7
Le Van Nghia <nghialv2607@gmail.com>
2012-12-15 16:25:38 +0900

    add config

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "hello world";
+  echo "hello world";
+
+  $config = parse_ini_file('config.ini', true);
+  $con = mysql_connect($config[ip], $config[username], $config[password]);
+  if(!$con){
+    echo('could not connect'.mysql_error());
+  }
+
+  $db_select = mysql_select_db($config[dbname], $con);
+  echo $db_select;
+
