1e60837dccd0c95c4e87f3fa96b960493d8d2936
Le Van Nghia <nghialv2607@gmail.com>
2012-12-16 01:31:56 +0900

    edit search form

--- login.php
+++ login.php
@@ -5,11 +5,14 @@
 
 <div id="container">
   <div id="main-container">
-  <?php
-    $authURL = 'https://www.facebook.com/dialog/oauth?client_id=' . APP_ID . '&redirect_uri=' . urlencode(CALLBACK) . '&scope=' . APP_SCOPE;
-    echo "<a href=$authURL>Login via facebook</a>";
-  ?>
+    <div class="transparent-container">
+    <?php
+      $authURL = 'https://www.facebook.com/dialog/oauth?client_id=' . APP_ID . '&redirect_uri=' . urlencode(CALLBACK) . '&scope=' . APP_SCOPE;
+      echo "<a href=$authURL>Login via facebook</a>";
+    ?>
+    </div>
   </div>
+  <div style="clear:both;"></div>
 </div>
 
 </body>
